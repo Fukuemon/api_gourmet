38a2caf6d52259b17377d410f826469f45088e50
Fukuemon <Fukuemony029@gmail.com>
2023-06-01 14:36:22 +0900

    ⚡️ユーザとプロフィールのシリアライザーを定義 #4
    シリアライザーは基本的に1つのモデルに1つ指定する
    
    ユーザーシリアライザー
    class Meta：
    シリアライザーが取り扱うモデルとその情報を設定する
    fields：パラメーターの指定
    extra_kwargs：パラメータのオプションを指定
    
    def create：
    models.pyで定義してるmodelのメソッドを使用して、ユーザを作成
    
    プロフィールシリアライザー
    created_on：フォーマートを定義する
    extra_kwargs：userProfileを読み取り専用に
    ログインしてるユーザーが投稿するため、ユーザーパラメータをDjango側で自動的に設定するようにする(後ほど)

--- api/models.py
+++ api/models.py
@@ -89,7 +89,7 @@ class User(AbstractBaseUser, PermissionsMixin):
 class Profile(models.Model):
     nickName = models.CharField(max_length=20)
 
-    user = models.OneToOneField(
+    userProfile = models.OneToOneField(
         settings.AUTH_USER_MODEL, related_name='profile',
         on_delete=models.CASCADE
     )
