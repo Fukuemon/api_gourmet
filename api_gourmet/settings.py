cf6b9a4a3e9c15625a29c4cb71aac57148b1aab7
Fukuemon <Fukuemony029@gmail.com>
2023-06-04 16:14:40 +0900

    ⚡ APIKEYを環境ファイルに移行
    python-decouple
    dj-database-url
    dj-static
    をimport
    
    settings.pyを書き換える

--- api_gourmet/settings.py
+++ api_gourmet/settings.py
@@ -12,6 +12,11 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 from datetime import timedelta
+from pathlib import Path
+from decouple import config
+from dj_database_url import parse as dburl
+
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -20,10 +25,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '4-02b)8-76ts(3kyex4!+pp2*en!jrsh7vmoex=8!4uk^n$)62'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG')
 
 ALLOWED_HOSTS = []
 
@@ -106,6 +111,12 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
+}
+
+default_dburl = 'sqlite:///' + str(BASE_DIR / "db.sqlite3")
+
+DATABASE = {
+    'defaulet': config('DATABASE_URL', default=default_dburl, cast=dburl )
 }
 
 
