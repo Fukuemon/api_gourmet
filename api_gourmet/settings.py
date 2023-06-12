837385040da3fba93a0ff48a07578c4f810afcd9
Fukuemon <Fukuemony029@gmail.com>
2023-06-13 00:33:45 +0900

    ♲corsのホワイトリスト修正

--- api_gourmet/settings.py
+++ api_gourmet/settings.py
@@ -64,7 +64,7 @@ MIDDLEWARE = [
 CORS_ORIGIN_WHITELIST = [
     "http://localhost:3000",
     "https://ar-gourmet-review.vercel.app",
-    "https://gourmet-ar-reviewapp.vercel.app/"
+    "https://gourmet-ar-reviewapp.vercel.app"
 ]
 
 
